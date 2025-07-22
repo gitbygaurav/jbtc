6938c45f44dc7d41a942c420fc3b4a3f294a9e44
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-07-22 10:31:36 +0530

    Add Navbar component with navigation links and logo integration

---
+++ src/app/buy/page.tsx
@@ -0,0 +1,9 @@
+const Page: React.FC = () => {
+    return (
+        <div className="px-2 lg:px-8">
+            <div className="flex "></div>
+        </div>
+    )
+}
+
+export default Page;
--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -1,14 +1,32 @@
+import Link from "next/link";
+
 const Navbar: React.FC = () => {
     return (
         <nav className="w-full px-2 lg:px-8 py-4 border-b-2 border-gray-200">
             <div className="flex justify-between items-center">
-                <img loading="lazy" src="/logo.png" alt="jbtc-logo" />
+                <Link href="/">
+                    <img loading="lazy" src="/logo.png" alt="jbtc-logo" className="cursor-pointer" />
+                </Link>
                 <ul className="hidden md:flex space-x-6 text-base">
-                    <li className="text-graytext">Home</li>
-                    <li>Dashboard</li>
-                    <li className="text-graytext">Buy</li>
+                    <li>
+                        <Link href="/" className="text-graytext hover:text-black transition-colors">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/dashboard" className="hover:text-graytext transition-colors">
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/buy" className="text-graytext hover:text-black transition-colors">
+                            Buy
+                        </Link>
+                    </li>
                 </ul>
-                <button className="bg-black text-white py-2 px-4 rounded-lg">{"fx34x...t6Vxv"}</button>
+                <button className="bg-black text-white py-2 px-4 rounded-lg hover:bg-gray-800 transition-colors">
+                    {"fx34x...t6Vxv"}
+                </button>
             </div>
         </nav>
     )
